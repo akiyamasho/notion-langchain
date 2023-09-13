1e0ad03da878108df4c86211f4c10b1a7f5bfa36
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2023-09-13 21:54:19 +0900

    FIX: use only one qdrant worker to avoid SQLite multithread error

--- run-qdrant.sh
+++ run-qdrant.sh
@@ -2,4 +2,6 @@
 
 docker run -p 6333:6333 \
     -v $(pwd)/qdrant_storage:/qdrant/storage \
-    qdrant/qdrant:v1.2.2
+    -v $(pwd)/qdrant_config.yaml:/qdrant/config/custom_config.yaml \
+    qdrant/qdrant:v1.2.2 \
+    ./qdrant --config-path config/custom_config.yaml
